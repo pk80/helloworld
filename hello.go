e3b04d0f83e160c5587df16788d26116bfc52e5f
Praveen <praveen.goprog@gmail.com>
2017-07-24 19:59:54 +0530

    modified hello.go

---
+++ hello.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+type people struct {
+	fname string
+	lname string
+}
+
+func main()  {
+	//fmt.Println("Hello world !")
+	p1:=people{"Praveen","Kumar"}
+	fmt.Println(p1)
+}
