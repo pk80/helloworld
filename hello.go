c8120705d0c356b0d4a9b31fc8e223f9fe073c92
Praveen <praveen.goprog@gmail.com>
2017-07-24 19:15:04 +0530

    change to master branch

--- hello.go
+++ hello.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	//"bufio"
+	//"os"
+)
 
 func main()  {
 	fmt.Println("Hello world !")
+	//reader:=bufio.NewReader(os.Stdin)
+	u1:=""
+	fmt.Println("Enter your name:")
+	//u1, _:=reader.ReadString('\n')
+	fmt.Scanln(&u1)
+	//fmt.Println(u1)
+	p1:=""
+	fmt.Println("Enter password:")
+	fmt.Scanln(&p1)
+	if u1=="Praveen" && p1=="abcdef"{
+		fmt.Println("login success...")
+	}else {
+		fmt.Println("login failed...")
+	}
 }
