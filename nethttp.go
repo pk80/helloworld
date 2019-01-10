adc6b2f85fd38f577b94e6219033a1e62ae37565
p <p@mulys-MacBook-Pro.local>
2019-01-10 19:34:10 +0530

    update

--- nethttp.go
+++ nethttp.go
@@ -1,12 +1,13 @@
 package main
 
-import(
-	"net/http"
+import (
 	"fmt"
 	"log"
+	"net/http"
 	//"reflect"
 	//"crypto/aes"
 )
+
 /*//Handler
 type helloHandler struct {
 
@@ -114,17 +115,17 @@ func newHeaderSetter(key,val string) func(http.Handler) http.Handler  {
 }
 func main(){
 	h:=http.NewServeMux()
-	
+
 	h.Handle("/one", numberDumper(1))
 	h.Handle("/nine",numberDumper(9))
-	
+
 	h.HandleFunc("/",func(w http.ResponseWriter, r *http.Request){
 		w.WriteHeader(404)
 		fmt.Fprintln(w,"\nThat's not a supported number!")
 	})
 	h1:=logger(h)
 	hhs:=newHeaderSetter("X-FOO","BAR")(h1)
-	
+
 	err:=http.ListenAndServe(":8080",hhs)
 	if err!=nil{
 		log.Fatalln(err)
@@ -133,46 +134,55 @@ func main(){
 //Composing middleware with alice
 type numberDumper int
 
-func (n numberDumper) ServeHTTP(w http.ResponseWriter, r *http.Request)  {
-	fmt.Fprintf(w,"Here's your number: %d\n",n)
+func (n numberDumper) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Here's your number: %d\n", n)
 }
-func logger(h http.Handler)http.Handler  {
-	return http.HandlerFunc(func(w http.ResponseWriter,r *http.Request) {
-		log.Printf("%s requested %s",r.RemoteAddr,r.URL)
-		h.ServeHTTP(w,r)
+func logger(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%s requested %s", r.RemoteAddr, r.URL)
+		h.ServeHTTP(w, r)
 	})
 }
+
 type headerSetter struct {
-		key,val string
-		handler http.Handler
+	key, val string
+	handler  http.Handler
 }
-func (hs headerSetter) ServeHTTP(w http.ResponseWriter,r *http.Request)  {
-	w.Header().Set(hs.key,hs.val)
-	hs.handler.ServeHTTP(w,r)
+
+func (hs headerSetter) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set(hs.key, hs.val)
+	hs.handler.ServeHTTP(w, r)
 }
-func newHeaderSetter(key,val string)func(handler http.Handler)http.Handler  {
+func newHeaderSetter(key, val string) func(handler http.Handler) http.Handler {
 	return func(h http.Handler) http.Handler {
-		return headerSetter{key,val,h}
+		return headerSetter{key, val, h}
 	}
 }
-func main(){
-	h:=http.NewServeMux()
-	h.Handle("/one",numberDumper(1))
-	h.Handle("/nine",numberDumper(9))
+func main() {
+	h := http.NewServeMux()
+	h.Handle("/one", numberDumper(1))
+	h.Handle("/two", numberDumper(2))
+	h.Handle("/three", numberDumper(3))
+	h.Handle("/four", numberDumper(4))
+	h.Handle("/five", numberDumper(5))
+	h.Handle("/six", numberDumper(6))
+	h.Handle("/seven", numberDumper(7))
+	h.Handle("/eight", numberDumper(8))
+	h.Handle("/nine", numberDumper(9))
 
-	tenHS:=newHeaderSetter("X-Five","First two digit number")
-	h.Handle("/ten",tenHS(numberDumper(10)))
+	tenHS := newHeaderSetter("X-Five", "First two digit number")
+	h.Handle("/ten", tenHS(numberDumper(10)))
 
 	h.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(404)
-		fmt.Fprintln(w,"That's not a supported number!")
+		fmt.Fprintln(w, "That's not a supported number!")
 	})
 
-//	chain:=alice.New(//alice is in new pakage in middleware
-//		newHeaderSetter("X-Foo","Bar"),
-//		newHeaderSetter("X-Baz","Buz"),
-//		logger,
-//	).Then (h)
-	err:=http.ListenAndServe(":8080",h)
+	//	chain:=alice.New(//alice is in new pakage in middleware
+	//		newHeaderSetter("X-Foo","Bar"),
+	//		newHeaderSetter("X-Baz","Buz"),
+	//		logger,
+	//	).Then (h)
+	err := http.ListenAndServe(":8080", h)
 	log.Fatal(err)
 }
